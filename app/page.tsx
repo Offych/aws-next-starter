5fb6c825acc608070c5fbba66f0260caf76cb34d
Alexander <alexandersmotrikov@gmail.com>
2025-07-22 14:23:16 +0300

    font small fix

--- app/page.tsx
+++ app/page.tsx
@@ -13,10 +13,10 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
+        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
+            <code className="bg-black/[.05] dark:bg-white/[.06] font-semibold px-1 py-0.5 rounded">
               app/page.tsx
             </code>
             .
