3d4720c96137b55dae79338c57d9626b07e28338
Alexander <alexandersmotrikov@gmail.com>
2025-07-23 16:05:27 +0300

    Small fix, remove next js start page

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
 import { SidebarTrigger } from "@/components/ui/sidebar"
 
@@ -13,7 +12,7 @@ export function SiteHeader() {
         />
         <h1 className="text-base font-medium">Documents</h1>
         <div className="ml-auto flex items-center gap-2">
-           Let's make something awesome!
+           Let&apos;s make something awesome!
         </div>
       </div>
     </header>
